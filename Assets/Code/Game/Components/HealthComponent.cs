903a99a2fc4f022242fe7a4aa78ade9b5ef146bc
Andrew Shibin <j3tmarch@gmail.com>
2024-06-15 19:54:34 +0300

    feat: add health component, remove game loop installer

---
+++ Assets/Code/Game/Components/HealthComponent.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+
+namespace ArenaShooter.Components
+{
+    public class HealthComponent : MonoBehaviour
+    {
+        [SerializeField]
+        private float _maxHealth;
+        [SerializeField] 
+        private float _minHealth;
+        [SerializeField]
+        private float _currentHealth;
+
+        public event Action<float> CurrentHealthChanged;
+        public event Action<float> MaxHealthChanged;
+
+        public float CurrentHealth {  get { return _currentHealth; } }
+        public float MaxHealth { get { return _maxHealth; } }
+        public float MinHealth { get { return _minHealth; } }
+
+        public void SetCurrentHealth(float health)
+        {
+            _currentHealth = health;
+            CurrentHealthChanged?.Invoke(_currentHealth);
+        }
+
+        public void SetMaxHealth(float health)
+        {
+            _maxHealth = health;
+            MaxHealthChanged?.Invoke(_maxHealth);
+        }
+    }
+}
