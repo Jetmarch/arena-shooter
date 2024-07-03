3cd5dd21586a2293bb2c94617f1ee463ba1375dd
Andrew Shibin <j3tmarch@gmail.com>
2024-07-03 18:15:28 +0300

    feat: sounds, serialized dict

---
+++ Assets/Code/Game/Audio/AudioManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace ArenaShooter.Audio
+{
+    public class AudioManager : MonoBehaviour
+    {
+        public AudioClip GetSound(string name)
+        {
+            return null;
+        }
+    }
+}
